05f5755e6e97a9f0c82d01017dc82d55a02e4ae4
umumumumumum <cjsteines404@gmail.com>
2022-01-30 22:29:44 -0700

    bruh

--- index.js
+++ index.js
@@ -16,5 +16,47 @@
 
 // Registration
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBHWI-peAX4h9garTvInmERXHeYI6vHhMY",
+  authDomain: "shogidatabase.firebaseapp.com",
+  databaseURL: "https://shogidatabase-default-rtdb.firebaseio.com",
+  projectId: "shogidatabase",
+  storageBucket: "shogidatabase.appspot.com",
+  messagingSenderId: "1028861476920",
+  appId: "1:1028861476920:web:1cd9b30aab2583d712a6cd",
+  measurementId: "G-NJJ9JXGL88"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+    document.getElementById('regform').addEventListener("submit", formSubmit);
+
+    function formSubmit(e) {
+        e.preventDefault();
+        let name = document.querySelector("#name").value;
+        let username = document.querySelector("#username").value;
+        let email = document.querySelector("#email").value;
+        let password = document.querySelector("#password").value;
+        let repassword = document.querySelector("#repassword").value;
+    }
+
+
+   
+
+ 
+    
+
     document.querySelector(".alert").style.display = "block";
     setTimeout(function() {document.querySelector(".alert").style.display = 'none';}, 7000);
